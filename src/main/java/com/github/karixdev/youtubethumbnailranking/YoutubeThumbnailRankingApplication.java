3aead7dc12eee8a65432447f37f655abf85492d6
karixdev <kari.klucz@gmail.com>
2022-12-08 18:13:51 +0100

    init

---
+++ src/main/java/com/github/karixdev/youtubethumbnailranking/YoutubeThumbnailRankingApplication.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.youtubethumbnailranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeThumbnailRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeThumbnailRankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/karixdev/youtubethumbnailranking/YoutubeThumbnailRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.karixdev.youtubethumbnailranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeThumbnailRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
