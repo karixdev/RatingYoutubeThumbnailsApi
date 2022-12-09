3105c185e834c7f2d3709c230e5bee26466c468d
karixdev <30869888+karixdev@users.noreply.github.com>
2022-12-09 21:33:02 +0100

    fix: verify that repository save method is called

--- src/test/java/com/github/karixdev/youtubethumbnailranking/user/UserServiceTest.java
+++ src/test/java/com/github/karixdev/youtubethumbnailranking/user/UserServiceTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -159,6 +160,7 @@ public class UserServiceTest {
 
         // Then
         assertThat(result).isEqualTo(result);
+        verify(userRepository).save(any());
     }
 
 }
