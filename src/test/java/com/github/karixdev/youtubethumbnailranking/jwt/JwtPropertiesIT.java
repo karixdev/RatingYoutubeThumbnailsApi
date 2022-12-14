e9f12ece54ba6d8e6ed9c2ee067b6528806fff33
karixdev <30869888+karixdev@users.noreply.github.com>
2022-12-14 21:10:33 +0100

    feat: add jwt service

---
+++ src/test/java/com/github/karixdev/youtubethumbnailranking/jwt/JwtPropertiesIT.java
@@ -0,0 +1,35 @@
+package com.github.karixdev.youtubethumbnailranking.jwt;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
+public class JwtPropertiesIT {
+    @Autowired
+    JwtProperties underTest;
+
+    @Test
+    void shouldLoadCorrectIssuer() {
+        assertThat(underTest.getIssuer())
+                .isEqualTo("youtube-thumbnail-ranking-test");
+    }
+
+    @Test
+    void shouldLoadCorrectTokenExpirationHours() {
+        assertThat(underTest.getTokenExpirationHours())
+                .isEqualTo(1);
+    }
+
+    @Test
+    void shouldCreateCorrectAlgorithm() {
+        assertThat(underTest.getAlgorithm().getName())
+                .isEqualTo("RS256");
+    }
+}
