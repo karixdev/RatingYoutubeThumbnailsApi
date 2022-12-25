9920ffc35f97f4d48b2a8f2139dc8e3748f5e9e3
karixdev <30869888+karixdev@users.noreply.github.com>
2022-12-25 19:43:08 +0100

    fix: add EmptyThumbnailsListException

---
+++ src/main/java/com/github/karixdev/youtubethumbnailranking/thumnail/exception/EmptyThumbnailsListException.java
@@ -0,0 +1,11 @@
+package com.github.karixdev.youtubethumbnailranking.thumnail.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class EmptyThumbnailsListException extends RuntimeException {
+    public EmptyThumbnailsListException() {
+        super("Provided list with thumbnails is empty");
+    }
+}
