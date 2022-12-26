cccfd375a915b259fd6f38bc11034b66a94a0068
karixdev <30869888+karixdev@users.noreply.github.com>
2022-12-26 21:04:42 +0100

    fix: change GameHasEndedException response status to 409

--- src/main/java/com/github/karixdev/youtubethumbnailranking/game/exception/GameHasEndedException.java
+++ src/main/java/com/github/karixdev/youtubethumbnailranking/game/exception/GameHasEndedException.java
@@ -1,5 +1,9 @@
 package com.github.karixdev.youtubethumbnailranking.game.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class GameHasEndedException extends RuntimeException {
     public GameHasEndedException() {
         super("Game has ended");
