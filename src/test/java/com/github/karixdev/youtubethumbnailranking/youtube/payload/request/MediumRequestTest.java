d10cc9771e6ccda70394400a4af5be1b4d67ebdf
karixdev <30869888+karixdev@users.noreply.github.com>
2022-12-27 18:02:01 +0100

    test: add missing youtube payload tests

---
+++ src/test/java/com/github/karixdev/youtubethumbnailranking/youtube/payload/request/MediumRequestTest.java
@@ -0,0 +1,33 @@
+package com.github.karixdev.youtubethumbnailranking.youtube.payload.request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class MediumRequestTest {
+    @Autowired
+    JacksonTester<MediumRequest> jTester;
+
+    @Test
+    void testDeserialize() throws IOException {
+        String payload = """
+                {
+                    "url": "url",
+                    "width": 100,
+                    "height": 100
+                }
+                """;
+
+        MediumRequest result = jTester.parseObject(payload);
+
+        assertThat(result.getUrl()).isEqualTo("url");
+        assertThat(result.getHeight()).isEqualTo(100);
+        assertThat(result.getWidth()).isEqualTo(100);
+    }
+}
