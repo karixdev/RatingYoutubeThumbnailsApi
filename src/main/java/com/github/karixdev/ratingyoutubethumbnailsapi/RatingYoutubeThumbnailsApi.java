563354299ba9e4beed75bb906fdd19302eac7af7
karixdev <30869888+karixdev@users.noreply.github.com>
2023-01-07 17:35:03 +0100

    style: rename project package to ratingyoutubethumbnailsapi

--- src/main/java/com/github/karixdev/ratingyoutubethumbnailsapi/RatingYoutubeThumbnailsApi.java
+++ src/main/java/com/github/karixdev/ratingyoutubethumbnailsapi/RatingYoutubeThumbnailsApi.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-public class RatingYoutubeThumbnails {
+public class RatingYoutubeThumbnailsApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RatingYoutubeThumbnails.class, args);
+		SpringApplication.run(RatingYoutubeThumbnailsApi.class, args);
 	}
 
 }
