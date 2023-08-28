ca3702f42733e41b02e4eea8d9ffec72b4ebc6ec
karixdev <kari.klucz@gmail.com>
2023-08-28 09:35:13 +0200

    feat: add swagger

--- src/main/java/com/github/karixdev/ratingyoutubethumbnailsapi/security/SecurityConfig.java
+++ src/main/java/com/github/karixdev/ratingyoutubethumbnailsapi/security/SecurityConfig.java
@@ -26,6 +26,21 @@ public class SecurityConfig {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -36,6 +51,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/email-verification/**").permitAll()
                         .requestMatchers("/api/v1/rating/**").permitAll()
                         .requestMatchers("/error").permitAll()
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
