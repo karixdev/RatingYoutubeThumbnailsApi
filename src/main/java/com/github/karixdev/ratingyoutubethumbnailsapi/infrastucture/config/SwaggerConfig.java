0d9708dd943728a08ff99ecf48e735031def593c
karixdev <kari.klucz@gmail.com>
2023-09-13 22:42:33 +0200

    feat: configure bearer auth in swagger

--- src/main/java/com/github/karixdev/ratingyoutubethumbnailsapi/infrastucture/config/SwaggerConfig.java
+++ src/main/java/com/github/karixdev/ratingyoutubethumbnailsapi/infrastucture/config/SwaggerConfig.java
@@ -1,15 +1,35 @@
 package com.github.karixdev.ratingyoutubethumbnailsapi.infrastucture.config;
 
+import com.github.karixdev.ratingyoutubethumbnailsapi.infrastucture.security.CurrentUser;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
     OpenAPI openAPI() {
         return new OpenAPI()
-                .info(new Info().title("Rating Youtube Thumbnails API"));
+                .info(new Info().title("Rating Youtube Thumbnails API"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", jwtScheme()));
     }
+
+    SecurityScheme jwtScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
+    static {
+        SpringDocUtils.getConfig().addAnnotationsToIgnore(AuthenticationPrincipal.class);
+    }
+
 }
